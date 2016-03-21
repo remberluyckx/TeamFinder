7876ae1867554cab0212efcdec18e6cb1abb4c7f
remberluyckx <remberluyckx@gmail.com>
2016-03-21 10:18:07 +0100

    added database branch for chat log instead of pushing everything to main branch + added html code for posts

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 $( document ).ready(function() {
 ///CHAT SYSTEM///
 var myDataRef = new Firebase('https://glowing-inferno-9012.firebaseio.com/');
+    var chatRef = myDataRef.child("chatlog");
 $('#messageInput').keypress(function (e) {
     if (e.keyCode == 13) {
         var name = $('#nameInput').val();
         var text = $('#messageInput').val();
         //myDataRef.set('User ' + name + ' says ' + text);
         //myDataRef.set({name: name, text: text}); // set vervangt telkens zijn vorige waarde
-        myDataRef.push({name: name, text: text}); // push append aan een list
+        chatRef.push({name: name, text: text}); // push append aan een list
         $('#messageInput').val('');
     }
 });
 
 // in Firebase, you always read data using callbacks
-myDataRef.on('child_added', function(snapshot) { // notify when chat messages arrive
+chatRef.on('child_added', function(snapshot) { // notify when chat messages arrive
     var message = snapshot.val();
     displayChatMessage(message.name, message.text);
 });
@@ -110,4 +111,8 @@ $('#forgotBtn').click(function () {
     });
 })
 ///////////////////
+
+///Post system////
+
+//////////////////
 });
