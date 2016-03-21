a828f2318aabb8d4d21ff0de78ab0bbed828ea71
samueru735 <samcontrol@hotmail.com>
2016-03-21 10:00:01 +0100

    login

--- app.js
+++ app.js
@@ -1,86 +1,50 @@
-$( document ).ready(function() {
-///CHAT SYSTEM///
-var myDataRef = new Firebase('https://glowing-inferno-9012.firebaseio.com/');
-    var chatRef = myDataRef.child("chatlog");
-$('#messageInput').keypress(function (e) {
-    if (e.keyCode == 13) { //keyCode 13 = enter
-        var name = $('#nameInput').val();
-        var text = $('#messageInput').val();
-        //myDataRef.set('User ' + name + ' says ' + text);
-        //myDataRef.set({name: name, text: text}); // set vervangt telkens zijn vorige waarde
-        chatRef.push({name: name, text: text}); // push append aan een list
-        $('#messageInput').val('');
-    }
+var app = angular.module("myApp", []);
+
+app.controller("appController", function ($scope) {
+
+//var myDataRef = new Firebase('https://glowing-inferno-9012.firebaseio.com/');
+
+var ref = new Firebase("https://boiling-torch-9537.firebaseio.com");
+
+$scope.loginMail = "";
+$scope.loginPassword = "";
+$scope.username = "Guest";
+$scope.authMessage = "";
+
+$scope.register = function()  {
+  ref.createUser({
+  email    : $scope.loginMail,
+  password : $scope.loginPassword
+}, function(error, userData) {
+  if (error) {
+    console.log("Error creating user:", error);
+  } else {
+    console.log("Successfully created user account with uid:", userData.uid);
+  }
 });
+  console.log("Login-mail: " + $scope.loginMail);
+  console.log("Login-password: " + $scope.loginPassword)
+}
 
-// in Firebase, you always read data using callbacks
-chatRef.on('child_added', function(snapshot) { // notify when chat messages arrive
-    var message = snapshot.val();
-    displayChatMessage(message.name, message.text);
+$scope.login = function () {
+  ref.authWithPassword({
+    email: $scope.loginMail,
+    password: $scope.loginPassword
+  }, function(error, authData) {
+      if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+    $scope.username = $scope.loginMail;
+  }
 });
-
-function displayChatMessage(name, text) {
-    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-};
-///////////////////
-
-    var email;
-    var password;
-///USER CREATION///
-$('#createBtn').click(function () {
-    email = $('#e-mailInput').val();
-    password = $('#passwordInput').val();
-    myDataRef.createUser({
-        email    : email,
-        password : password
-    }, function(error, userData) {
-        if (error) {
-            console.log("Error creating user:", error);
-        } else {
-            console.log("Successfully created user account with uid:", userData.uid);
-        }
-    });
-})
-///////////////////
-
-////USER LOGIN/////
-$('#loginBtn').click( function() {
-    email = $('#e-mailInput').val();
-    password = $('#passwordInput').val();
-    myDataRef.authWithPassword({
-        email    : email,
-        password : password
-    }, function(error, authData) {
-        if (error) {
-            console.log("Login Failed!", error);
-        } else {
-            console.log("Authenticated successfully with payload:", authData);
-        }
-    });
-})
-///////////////////
-
-////USER DELETE////
-$('#deleteBtn').click( function() {
-    email = $('#e-mailInput').val();
-    password = $('#passwordInput').val();
-    myDataRef.removeUser({
-        email    : email,
-        password : password
-    }, function(error) {
-        if (error === null) {
-            console.log("User removed successfully");
-        } else {
-            console.log("Error removing user:", error);
-        }
-    });
-})
+  
+}
 ///Password change///
 $('#changeBtn').click( function() {
-    email = $('#e-mailInput').val();
-    password = $('#passwordInput').val();
-    var newPass = $('#newPassInput').val();
+    email = $scope.loginMail;
+    password = $scope.loginPassword;
+    var newPass = $scope.newPassword;
     myDataRef.changePassword({
         email       : email,
         oldPassword : password,
@@ -94,13 +58,28 @@ $('#changeBtn').click( function() {
         }
     });
 })
-////////////////////
+
+////USER DELETE////
+$('#deleteBtn').click( function() {
+    email = $scope.loginMail;
+    password = $scope.loginPassword;
+    ref.removeUser({
+        email    : email,
+        password : password
+    }, function(error) {
+        if (error === null) {
+            console.log("User removed successfully");
+        } else {
+            console.log("Error removing user:", error);
+        }
+    });
+})
 
 ///Password reset///
 $('#forgotBtn').click(function () {
-    email = $('#e-mailInput').val();
-    password = $('#passwordInput').val();
-    myDataRef.resetPassword({
+    email = $scope.loginMail;
+    password = $scope.loginPassword
+    ref.resetPassword({
         email : email
     }, function(error) {
         if (error === null) {
@@ -110,6 +89,7 @@ $('#forgotBtn').click(function () {
         }
     });
 })
+
 ///////////////////
 
 ////Post system////
@@ -137,3 +117,36 @@ $("#postBtn").click( function() {
     };
 //////////////////
 });
+
+// Chat system //
+
+$('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          ref.push({name:name, text:text});
+          $('#messageInput').val('');
+        }
+      });
+ref.on('child_added', function(snapshot) {
+         var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
+  
+});
+
+/*myDataRef.authWithOAuthPopup("github", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+}, {
+    remember: "sessionOnly",
+      scope: "user"
+}); */
+
