020a1c3b80a498e2009781c82d0f92942c5e7f5e
remberluyckx <remberluyckx@gmail.com>
2016-05-09 11:12:06 +0200

    Added facebook sign up

--- app.js
+++ app.js
@@ -4,290 +4,261 @@ app.controller("appController", function ($scope) {
 
 //var ref = new Firebase('https://glowing-inferno-9012.firebaseio.com/');
 
-var ref = new Firebase("https://boiling-torch-9537.firebaseio.com");
-var usersRef = ref.child("users");
-var chatRef = ref.child("chat");
+    var ref = new Firebase("https://boiling-torch-9537.firebaseio.com");
+    var usersRef = ref.child("users");
+    var chatRef = ref.child("chat");
 
-$scope.loginMail = "";
-$scope.loginPassword = "";
-$scope.username = "guest";
-$scope.authMessage = "";
+    $scope.loginMail = "";
+    $scope.loginPassword = "";
+    $scope.username = "guest";
+    $scope.authMessage = "";
 
-$scope.uid = "";
+    $scope.uid = "";
 
-var uID = "";
+    var uID = "";
 
-var getUserName = function(){
-  usersRef.orderByChild("uid").equalTo(uID.toString()).on("child_added", function(snapshot){
-    key = snapshot.key();
-    $scope.user = snapshot.val();
-    if($scope.user){
-      $scope.username =  $scope.user.first_name + " " + $scope.user.last_name;  
-      $scope.$digest();
-    }
-    });     
-}
-
-var getUser = function(){
-  usersRef.orderByChild("uid").equalTo(uID.toString()).on("child_added", function(snapshot){
-    key = snapshot.key();
-    $scope.user = snapshot.val();
-  });
-}
-
-
-var checkIfRegistered =  function (uID) {
-  usersRef.orderByChild("uid").equalTo(uID).on("child_added", function(snapshot){
-    console.log("user already exists");
-    //uID = snapshot.val().uid; 
-    return true;
-  })    
-}
-
-if(ref.getAuth()){
-  uID = ref.getAuth().uid;
-  $scope.uid = uID;
-  getUserName();
-  console.log("user email:", ref.getAuth().email);
-}
-console.log("uid", uID);
-
-$scope.register = function()  {
-  ref.createUser({
-  email    : $scope.loginMail,
-  password : $scope.loginPassword
-}, function(error, userData) {
-  if (error) {
-    console.log("Error creating user:", error);
-  } else {
-    console.log("Successfully created user account with uid:", userData.uid);
-    usersRef.push({
-            uid: userData.uid,
-            email: $scope.loginMail
+    var getUserName = function () {
+        usersRef.orderByChild("uid").equalTo(uID.toString()).on("child_added", function (snapshot) {
+            key = snapshot.key();
+            $scope.user = snapshot.val();
+            if ($scope.user) {
+                $scope.username = $scope.user.first_name + " " + $scope.user.last_name;
+                $scope.$digest();
+            }
         });
-        console.log("saved new empty profile");
-  }
-});
-  console.log("Login-mail: " + $scope.loginMail);
-  console.log("Login-password: " + $scope.loginPassword)
-}
+    }
+
+    var getUser = function () {
+        usersRef.orderByChild("uid").equalTo(uID.toString()).on("child_added", function (snapshot) {
+            key = snapshot.key();
+            $scope.user = snapshot.val();
+        });
+    }
+
+
+    var checkIfRegistered = function (uID) {
+        usersRef.orderByChild("uid").equalTo(uID).on("child_added", function (snapshot) {
+            console.log("user already exists");
+            //uID = snapshot.val().uid;
+            return true;
+        })
+    }
+
+    if (ref.getAuth()) {
+        uID = ref.getAuth().uid;
+        $scope.uid = uID;
+        getUserName();
+        console.log("user email:", ref.getAuth().email);
+    }
+    console.log("uid", uID);
+
+    $scope.register = function () {
+        ref.createUser({
+            email: $scope.loginMail,
+            password: $scope.loginPassword
+        }, function (error, userData) {
+            if (error) {
+                console.log("Error creating user:", error);
+            } else {
+                console.log("Successfully created user account with uid:", userData.uid);
+                usersRef.push({
+                    uid: userData.uid,
+                    email: $scope.loginMail
+                });
+                console.log("saved new empty profile");
+            }
+        });
+        console.log("Login-mail: " + $scope.loginMail);
+        console.log("Login-password: " + $scope.loginPassword)
+    }
 
 //Login
-$scope.login = function () {
-  ref.authWithPassword({
-    remember: "sessionOnly",
-    email: $scope.loginMail,
-    password: $scope.loginPassword
-  },
-   function(error, authData) {
-      if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    uID = authData.uid;
-    console.log("Authenticated successfully with payload:" +uID, authData);
-    console.log("uid: " + uID);
-    $scope.$digest();
-    getUserName();
-  }
-});  
-  //$scope.$digest();
-}
+    $scope.login = function () {
+        ref.authWithPassword({
+                remember: "sessionOnly",
+                email: $scope.loginMail,
+                password: $scope.loginPassword
+            },
+            function (error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    uID = authData.uid;
+                    console.log("Authenticated successfully with payload:" + uID, authData);
+                    console.log("uid: " + uID);
+                    $scope.$digest();
+                    getUserName();
+                }
+            });
+        //$scope.$digest();
+    }
 
 //logout
-$scope.logout = function(){
-  ref.unauth();
-  $scope.user = null;
-  $scope.username = "guest";
-  console.log("logged out");
-  // google sign out
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-      console.log('User signed out.');
-  });
-}
+    $scope.logout = function () {
+        ref.unauth();
+        $scope.user = null;
+        $scope.username = "guest";
+        console.log("logged out");
+        // google sign out
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+    }
 
 ///Password change///
-$('#changeBtn').click( function() {
-    email = $scope.loginMail;
-    password = $scope.loginPassword;
-    var newPass = $scope.newPassword;
-    ref.changePassword({
-        email       : email,
-        oldPassword : password,
-        newPassword : newPass
-    }, function(error) {
-        if (error === null) {
-            console.log("Password changed successfully");
-            password = newPass;
-        } else {
-            console.log("Error changing password:", error);
-        }
-    });
-})
+    $('#changeBtn').click(function () {
+        email = $scope.loginMail;
+        password = $scope.loginPassword;
+        var newPass = $scope.newPassword;
+        ref.changePassword({
+            email: email,
+            oldPassword: password,
+            newPassword: newPass
+        }, function (error) {
+            if (error === null) {
+                console.log("Password changed successfully");
+                password = newPass;
+            } else {
+                console.log("Error changing password:", error);
+            }
+        });
+    })
 
 ////USER DELETE////
-/*$('#deleteBtn').click( function() {
-    email = $scope.loginMail;
-    password = $scope.loginPassword;
-    ref.removeUser({
-        email    : email,
-        password : password
-    }, function(error) {
-        if (error === null) {
-            console.log("User removed successfully");
-        } else {
-            console.log("Error removing user:", error);
-        }
-    });
-}) */
+    /*$('#deleteBtn').click( function() {
+     email = $scope.loginMail;
+     password = $scope.loginPassword;
+     ref.removeUser({
+     email    : email,
+     password : password
+     }, function(error) {
+     if (error === null) {
+     console.log("User removed successfully");
+     } else {
+     console.log("Error removing user:", error);
+     }
+     });
+     }) */
 
 ///Password reset///
-$('#forgotBtn').click(function () {
-    email = $scope.loginMail;
-    password = $scope.loginPassword;
-    ref.resetPassword({
-        email : email
-    }, function(error) {
-        if (error === null) {
-            console.log("Password reset email sent successfully");
-        } else {
-            console.log("Error sending password reset email:", error);
-        }
-    });
-})
+    $('#forgotBtn').click(function () {
+        email = $scope.loginMail;
+        password = $scope.loginPassword;
+        ref.resetPassword({
+            email: email
+        }, function (error) {
+            if (error === null) {
+                console.log("Password reset email sent successfully");
+            } else {
+                console.log("Error sending password reset email:", error);
+            }
+        });
+    })
 
 // Chat system //
 
-$('#messageInput').keypress(function (e) {
-        if (e.keyCode == 13 && $('#messageInput').val() != "") {          
-          var text = $('#messageInput').val();                    
-          chatRef.push({name:$scope.username, text:text});
-          $('#messageInput').val('');
+    $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13 && $('#messageInput').val() != "") {
+            var text = $('#messageInput').val();
+            chatRef.push({name: $scope.username, text: text});
+            $('#messageInput').val('');
         }
-      });
-chatRef.limitToLast(6).on('child_added', function(snapshot) {
-         var message = snapshot.val();
+    });
+    chatRef.limitToLast(6).on('child_added', function (snapshot) {
+        var message = snapshot.val();
         displayChatMessage(message.name, message.text);
-      });
-      function displayChatMessage(name, text) {
-        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    });
+    function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name + ': ')).appendTo($('#messagesDiv'));
         $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-      };
-
-// Google signin 
-$scope.googleLogin = function() {
-
-              console.log("in google sign in function");
-
-              ref.authWithOAuthPopup("google", function(error, authData) {
-              if (error) {
-                console.log("Login Failed!", error);
-              } else {
-                console.log("Authenticated successfully with payload:", authData);
-                uID = authData.uid;
-                if(checkIfRegistered(uID) === false){
-                  addGoogleUser(authData.google.cachedUserProfile);                
-                }
-                $scope.username = authData.google.displayName;
-                  
-                getUser();
-                $scope.$apply();
-               // getGoogleUserName();
-              }
-                         
-            },
-            {
-              remember: "sessionOnly",
-              scope:"email"
-            }
-
-            );
-        
-};
-var addGoogleUser = function(profile){
-  usersRef.push({
-
-        first_name: profile.given_name,
-        last_name: profile.family_name,
-        email: profile.email,
-        uid: uID
-  })
-  console.log("User created");
-}
-
-    /////Facebook API//////
-
-// This is called with the results from from FB.getLoginStatus().
-    function statusChangeCallback(response) {
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        if (response.status === 'connected') {
-            testAPI();
-            console.log("Logged into your app and Facebook");
-        } else if (response.status === 'not_authorized') {
-            document.getElementById('status').innerHTML = 'Please log ' +
-                'into this app.';
-            console.log("The person is logged into Facebook, but not your app");
-        } else {
-            document.getElementById('status').innerHTML = 'Please log ' +
-                'into Facebook.';
-            console.log("The person is not logged into Facebook, so we're not sure if they are logged into this app or not");
-        }
-    }
-
-// This function is called when someone finishes with the Login Button
-
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '2050929358466509',
-            cookie     : true,  // enable cookies to allow the server to access the session
-            xfbml      : true,  // parse social plugins on this page
-            version    : 'v2.5' // use graph api version 2.5
-        });
-
-        // Now that we've initialized the JavaScript SDK, we call
-        // FB.getLoginStatus().  This function gets the state of the
-        // person visiting this page and can return one of three states to
-        // the callback you provide.  They can be:
-        // 1. Logged into your app ('connected')
-        // 2. Logged into Facebook, but not your app ('not_authorized')
-        // 3. Not logged into Facebook and can't tell if they are logged into your app or not.
-        // These three cases are handled in the callback function.
-
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-
     };
 
-// Load the SDK asynchronously
-    (function(d, s, id) {
+    // Facebook like button
+    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.6&appId=2050929358466509";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
-    function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
-            console.log('Successful login for: ' + response.name);
-            document.getElementById('status').innerHTML =
-                'Thanks for logging in, ' + response.name + '!';
+    $scope.fblogin = function () {
+        //var ref = new Firebase("https://boiling-torch-9537.firebaseio.com");
+        ref.authWithOAuthPopup("facebook", function (error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                console.log(authData.facebook.cachedUserProfile.first_name);
+                uID = authData.uid;
+                if (checkIfRegistered(uID) === false) {
+                    addFacebookUser(authData.facebook.cachedUserProfile);
+                    console.log("added account");
+                }
+                else {
+                    console.log("account already exists");
+                }
+                $scope.username = authData.facebook.displayName;
+                getUser();
+                $scope.$apply();
+            }
         });
     }
-    //Callback when user logs in with Facebook
-    window.loggedIn = function () {
-        //console.log("Logged in!");
-        testAPI();
+
+// Google signin 
+    $scope.googleLogin = function () {
+
+        console.log("in google sign in function");
+
+        ref.authWithOAuthPopup("google", function (error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                    uID = authData.uid;
+                    if (checkIfRegistered(uID) === false) {
+                        addGoogleUser(authData.google.cachedUserProfile);
+                    }
+                    $scope.username = authData.google.displayName;
+
+                    getUser();
+                    $scope.$apply();
+                    // getGoogleUserName();
+                }
+
+            },
+            {
+                remember: "sessionOnly",
+                scope: "email"
+            }
+        );
+
+    };
+    var addGoogleUser = function (profile) {
+        usersRef.push({
+
+            first_name: profile.given_name,
+            last_name: profile.family_name,
+            email: profile.email,
+            uid: uID
+        })
+        console.log("User created");
     }
 
+    var addFacebookUser = function (profile) {
+        usersRef.push({
 
-///////////////////////
+            first_name: profile.first_name,
+            last_name: profile.last_name,
+            email: profile.email,
+            uid: uID
+        })
+        console.log("User created");
+    }
 
- });
+});
 
 /*ref.authWithOAuthPopup("github", function(error, authData) {
   if (error) {
